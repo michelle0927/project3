ab9e353b16fbb35693832227623c49c9c37b17ae
michelle0927 <michellelbergero@hotmail.com>
2015-08-08 23:25:54 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+frontend-nanodegree-arcade-game
+===============================
+
+The object of the game is to get the player to the water at the top of the screen.
+
+Keyboard keys to use:
+up arrow - moves the player up one space
+down arrow - moves the player down one space
+left arrow - moves the player to the left one space
+right arrow - moves the player to the right one space
+
+Avoid the bugs! If the player touches one of the bugs moving across the screen, the player will be reset to the starting location.
+
+Get to the water! When the player reaches the water, the score is increased by one, and the player is reset to the starting location to continue playing.
+
+Have fun!
---
+++ js/app.js
@@ -0,0 +1,119 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    this.x = -100;
+    this.row = Math.floor(Math.random() * 3) + 1;
+    if (this.row == 1)
+        this.y = 60;
+    else if (this.row ==2)
+        this.y = 145;
+    else if (this.row == 3)
+        this.y = 230;
+    this.speed = Math.floor(Math.random()*300) + 100;
+    // The image/sprite for our enemies
+    this.sprite = 'images/enemy-bug.png';
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505)
+        this.x = -100;
+    // if a collision happens
+    if (player.row == this.row && (player.x + 71) >= this.x && (player.x + 30) <= (this.x + 101))
+        player.collision = true;
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Player class
+var Player = function() {
+    this.x = 200;
+    this.y = 380;
+    this.row = 5;
+    this.collision = false;
+    this.score = 0;
+    this.sprite = "images/char-boy.png";
+}
+
+// Update player to starting position if a collision happens
+Player.prototype.update = function() {
+    if (this.collision == true)
+        this.reset();
+}
+
+// Draw the player and score on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // draw the score
+    ctx.fillStyle = "black";
+    ctx.fillRect(8, 560, 75, 20);
+    ctx.font = "17px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + this.score, 12, 575);
+}
+
+// Respond to arrow inputs from user. Player cannot move off the screen.
+Player.prototype.handleInput = function(key) {
+    if (key == 'left' && this.x > 0)
+        this.x -= 100;
+    else if (key == 'right' && this.x < 400)
+        this.x += 100;
+    else if (key == 'up' && this.y > 52) {
+        this.y -= 82;
+        this.row -= 1;
+    } else if (key == 'up' && this.y <= 52) { // made it to the water
+        this.reset();
+        this.increaseScore();
+    } else if (key == 'down' && this.y < 380) {
+        this.y += 82;
+        this.row += 1;
+    }
+}
+
+// Reset player to starting position
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 380;
+    this.row = 5;
+    this.collision = false;
+}
+
+// Increase the score on the screen if the player makes it to the water.
+Player.prototype.increaseScore = function() {
+    this.score += 1;
+    ctx.fillStyle = "black";
+    ctx.fillRect(8, 560, 75, 20);
+    ctx.font = "17px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + this.score, 12, 575);
+}
+
+// Create enemies and player
+
+var allEnemies = [];
+for (var i = 0; i < 4; i++) {
+    var e = new Enemy;
+    allEnemies.push(e);
+}
+
+var player = new Player;
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
